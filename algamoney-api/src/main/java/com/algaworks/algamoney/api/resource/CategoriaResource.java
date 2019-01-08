26369c5ea9e104eb5ce7e741ffff02fea78e2403
Gustavo Clay <gustavo.lima@eigmercados.com.br>
2019-01-08 12:03:40 -0200

    Curso Algaworks fullstack Angular Spring continuar aula 3.8

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -3,10 +3,12 @@ package com.algaworks.algamoney.api.resource;
 import com.algaworks.algamoney.api.model.Categoria;
 import com.algaworks.algamoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,7 +19,25 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 
 	@GetMapping
-	public List<Categoria> listar(){
+	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+						.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscaPeloCodigo(@PathVariable long codigo) {
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+	}
 }
+
