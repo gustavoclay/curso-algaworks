dabd07e2aaf71b6232b093608fd6ed1d7985a47f
Gustavo Clay <gustavoclay@gmail.com>
2019-01-15 18:19:45 -0200

    Curso Algaworks fullstack Angular Spring continuar 6.11. Movendo o usuário para o banco de dados

---
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
@@ -0,0 +1,49 @@
+package com.algaworks.algamoney.api.cors;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+	private String originPermitida = "http://localhost:8000"; // TODO: Configurar para diferentes ambientes
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+					throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		response.setHeader("Access-Control-Allow-Origin", originPermitida);
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+
+		if ("OPTIONS".equals(request.getMethod())) {
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			response.setHeader("Access-Control-Max-Age", "3600");
+
+
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, res);
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
