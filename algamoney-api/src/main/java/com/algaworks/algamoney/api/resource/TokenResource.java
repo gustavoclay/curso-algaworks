2eda32f845d5d78c67519f742337eb105518f092
Gustavo Clay <gustavoclay@gmail.com>
2019-01-16 14:54:00 -0200

    Curso Algaworks fullstack Angular Spring continuar 7.1. Implementando projeção de lançamento

---
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/resource/TokenResource.java
@@ -0,0 +1,28 @@
+package com.algaworks.algamoney.api.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+	@DeleteMapping("/revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse res){
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(false); // TODO: em produção será true
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+
+		res.addCookie(cookie);
+		res.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+
+}
